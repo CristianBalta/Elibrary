1cf66e91df36df59f4decd94912cea485a16e034
cristianbalta <baltacristiandorin@yahoo.com>
2020-02-16 03:14:56 +0200

    o scriere de imprumut rudimentara

--- src/main/java/ro/iteahome/eLibrary/Application.java
+++ src/main/java/ro/iteahome/eLibrary/Application.java
@@ -1,10 +1,15 @@
 package ro.iteahome.eLibrary;
 
+
+import ro.iteahome.eLibrary.service.LoanWriter;
 import ro.iteahome.eLibrary.ui.MainUI;
 
-public class Application {
-    public static void main(String[] args) {
+import java.io.IOException;
 
+public class Application {
+    public static void main(String[] args) throws IOException {
+
+        LoanWriter loanWriter = new LoanWriter(); //unde initializam clasa LoanWriter acolo va incepe un UI care in fucntie de detaliile introduse va creea un imprumut
 
         MainUI mainUI = new MainUI();
         mainUI.showTitle();
