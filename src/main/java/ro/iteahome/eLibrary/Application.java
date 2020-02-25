c23d1e57fb04c3cc6d336056bf67a827b83a0c1b
cristianbalta <baltacristiandorin@yahoo.com>
2020-02-25 08:27:53 +0200

    Finished LoanWriter for the admin

--- src/main/java/ro/iteahome/eLibrary/Application.java
+++ src/main/java/ro/iteahome/eLibrary/Application.java
@@ -2,6 +2,7 @@ package ro.iteahome.eLibrary;
 
 
 import ro.iteahome.eLibrary.service.LoanWriter;
+import ro.iteahome.eLibrary.ui.LoanWriterUI;
 import ro.iteahome.eLibrary.ui.MainUI;
 
 import java.io.IOException;
@@ -9,7 +10,8 @@ import java.io.IOException;
 public class Application {
     public static void main(String[] args) throws IOException {
 
-        LoanWriter loanWriter = new LoanWriter(); //unde initializam clasa LoanWriter acolo va incepe un UI care in fucntie de detaliile introduse va creea un imprumut
+        LoanWriterUI loanWriterUI = new LoanWriterUI(); //doar de test aici! unde o sa creeam un obiect de tip LoanWriter,
+                                                        // acolo putem initializa loan-ul deci o sa fie o optiune a adminuli mai incolo
 
         MainUI mainUI = new MainUI();
         mainUI.showTitle();
--- src/main/java/ro/iteahome/eLibrary/service/LoanWriter.java
+++ src/main/java/ro/iteahome/eLibrary/service/LoanWriter.java
@@ -1,104 +1,22 @@
 package ro.iteahome.eLibrary.service;
 
-import java.io.*;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Scanner;
+import ro.iteahome.eLibrary.dao.LoanDao;
 
-import ro.iteahome.eLibrary.model.Author;
-import ro.iteahome.eLibrary.model.Book;
-import ro.iteahome.eLibrary.model.Loan;
+
+import java.io.IOException;
 
 
 public class LoanWriter {
 
-    private static final String LOANS_FILE = "/Users/cristianbalta/Desktop/team-project-elibrary/src/main/java/ro/iteahome/eLibrary/loans.txt";
+
+    LoanDao loandaoui;
+
+    public LoanWriter(LoanDao loandaoui) throws IOException {
+        this.loandaoui = loandaoui;
+        this.loandaoui.loanToFile();
 
 
-    public LoanWriter() throws IOException {
-
-        Loan loan = new Loan();
-        loan.setLoanDate(new Date());
-
-        Scanner s = new Scanner(System.in);
-
-        int index = 0;
-        String lastLineIndex = null;
-        try (
-                FileReader reader = new FileReader(LOANS_FILE);
-                BufferedReader br = new BufferedReader(reader)) {
-            String line = null;
-
-            while ((line = br.readLine()) != null) {
-                lastLineIndex = line.substring(0, 1);
-            }
-
-
-            index = Integer.parseInt(lastLineIndex);
-        } catch (
-                IOException e) {
-            System.err.format("IOException: %s%n", e);
-        }
-
-        loan.setLoanId(index + 1);
-
-        System.out.println("What is the id of the reader?");
-        loan.setUserId(s.nextInt());
-        s.nextLine();
-        System.out.println("How many books will be in this loan?");
-        int numberOfBooks = s.nextInt();
-        s.nextLine();
-
-        List<Book> bookList = new ArrayList<>();
-
-        for (
-                int i = 0;
-                i < numberOfBooks; i++) {
-            Book book = new Book();
-
-            System.out.println("What is the name of the book?");
-            String bookName = s.nextLine();
-            book.setName(bookName);
-
-            System.out.println("How many authors does this book have?");
-            int numberOfAuthors = s.nextInt();
-            s.nextLine();
-            List<Author> authors = new ArrayList<>();
-
-            for (int j = 0; j < numberOfAuthors; j++) {
-                Author author = new Author();
-                System.out.println("What is the name of the author?");
-                String authorName = s.nextLine();
-                author.setName(authorName);
-                authors.add(author);
-            }
-            book.setAuthorList(authors);
-            bookList.add(book);
-        }
-        loan.setBookList(bookList);
-
-        try (
-                FileWriter writer = new FileWriter(LOANS_FILE, true);
-                BufferedWriter bw = new BufferedWriter(writer)) {
-
-
-            DateFormat dateFormat = new SimpleDateFormat("dd-MM-YYYY");
-            String strDate = dateFormat.format(loan.getLoanDate());
-
-
-            String loanToRegister = String.valueOf(loan.getLoanId()) + "; " + loan.getUserId() + "; " + strDate + "; " + loan.getBookList() + "\n";
-            bw.write(loanToRegister);
-
-        } catch (
-                IOException e) {
-            System.err.format("IOException: %s%n", e);
-        }
     }
-
-
 
 
 }
